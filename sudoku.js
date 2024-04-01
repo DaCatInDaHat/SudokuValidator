ea6258acca5773bd968397bb908ca6f80024f9b3
DaCatInDaHat <volodymyrkuznetsov87@gmail.com>
2024-04-01 23:19:30 +0300

    first commit

---
+++ sudoku.js
@@ -0,0 +1,64 @@
+const arr1 = [
+  [5, 3, 4, 6, 7, 8, 9, 1, 2],
+  [6, 7, 2, 1, 9, 5, 3, 4, 8],
+  [1, 9, 8, 3, 4, 2, 5, 6, 7],
+  [8, 5, 9, 7, 6, 1, 4, 2, 3],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 9, 2, 4, 8, 5, 6],
+  [9, 6, 1, 5, 3, 7, 2, 8, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 4, 5, 2, 8, 6, 1, 7, 9],
+]; //true expected
+
+const arr2 = [
+  [5, 3, 4, 6, 7, 8, 9, 1, 2],
+  [6, 7, 2, 1, 9, 0, 3, 4, 8],
+  [1, 0, 0, 3, 4, 2, 5, 6, 0],
+  [8, 5, 9, 7, 6, 1, 0, 2, 0],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 9, 2, 4, 8, 5, 6],
+  [9, 0, 1, 5, 3, 7, 2, 1, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 0, 0, 4, 8, 1, 1, 7, 9],
+]; //false expected
+
+const arr3 = [
+  [5, 5, 4, 6, 7, 8, 9, 1, 2],
+  [6, 7, 2, 1, 9, 5, 3, 4, 8],
+  [1, 9, 8, 3, 4, 2, 5, 6, 7],
+  [8, 5, 9, 7, 6, 1, 4, 2, 3],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 9, 2, 4, 8, 5, 6],
+  [9, 6, 1, 5, 3, 7, 2, 8, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 4, 5, 2, 8, 6, 1, 7, 9],
+]; //false expected
+
+function validSolution(board) {
+  let rows = Array.from(Array(9), () => []);
+  let columns = Array.from(Array(9), () => []);
+  let boxes = Array.from(Array(9), () => []);
+
+  for (let row in board) {
+    for (let column in board) {
+      let cell = board[row][column];
+
+      if (cell !== 0) {
+        if (rows[row].includes(cell)) return false;
+        else rows[row].push(cell);
+
+        if (columns[column].includes(cell)) return false;
+        else columns[column].push(cell);
+
+        let boxIndex = Math.floor(row / 3) * 3 + Math.floor(column / 3);
+
+        if (boxes[boxIndex].includes(cell)) return false;
+        else boxes[boxIndex].push(cell);
+      } else return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(validSolution(arr3));
